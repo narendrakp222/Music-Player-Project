a0009d0e46d7336e762ca8506a33574156607174
narendra kumar paswan <narendrakumarpaswan13@gmail.com>
2025-08-08 11:40:37 +0530

    just comit

---
+++ script.js
@@ -0,0 +1,73 @@
+// let audio=document.querySelector("audio")
+
+// let audio = document.createElement("audio"); 
+// audio.src = "Saiyaara Title Track Mp3 Song 2025 singer by Tanishk Bagchi, Faheem Abdullah, Arslan Nizami, Irshad Kamil from Saiyaara 2025.mp3"
+// let bodyref = document.querySelector(".player"); 
+
+// audio.volume = 0.5; // 0.0 to 1.0
+// bodyref.appendChild(audio); 
+
+//    audio.addEventListener("timeupdate", () => {
+//     const timeline = document.querySelector(".timeline");
+//     timeline.max = audio.duration;
+//     timeline.value = audio.currentTime;
+//   });
+
+
+// function play() {
+//   audio.play();                    
+// }
+
+// function increase(){
+
+//     audio.volume=Math.min(1,audio.volume + 0.1);
+
+
+// }
+
+// function decrease(){
+
+//     audio.volume=Math.max(0,audio.volume-0.1);
+// }
+
+// function seekAudio(input) {
+//     audio.currentTime = input.value;
+//   }
+
+
+
+let audio=document.createElement("audio");
+let player=document.querySelector(".player");
+audio.src="Saiyaara Title Track Mp3 Song 2025 singer by Tanishk Bagchi, Faheem Abdullah, Arslan Nizami, Irshad Kamil from Saiyaara 2025.mp3"
+audio.volume=0.5;
+player.prepend(audio);
+
+function play(){
+
+  audio.play();
+
+
+}
+
+function increase(){
+
+  audio.volume=Math.min(1,audio.volume+0.1);
+
+}
+
+function decrease(){
+
+  audio.volume=Math.max(0,audio.volume-0.1);
+
+}
+
+
+
+
+
+
+
+
+
+
+
